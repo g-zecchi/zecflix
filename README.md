291774dc535ef8ee34303c1b44887627f43a1bcb
Gabriel Zecchi <g.zecchi@hotmail.com>
2022-02-28 19:01:52 -0300

    Finalização do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# RECRIANDO INTERFACE DA NETFLIX
+
+## Desafio desenvolvido durante aprendizado do bootcamp webdeveloper da DIO
---
+++ js/owl/setup.js
@@ -0,0 +1,33 @@
+$('.owl-carousel1').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+$('.owl-carousel2').owlCarousel({
+    loop: false,
+    margin: 5,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
